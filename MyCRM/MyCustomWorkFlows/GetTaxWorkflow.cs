e087b0f8c4482c552f5436bcffab08f3b8e6e26b
Jason James <jason.james@cba-japan.com>
2026-07-28 22:13:32 +0100

    start of reviewing custom workflows

---
+++ MyCRM/MyCustomWorkFlows/GetTaxWorkflow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System.Activities;
+using System.Threading;
+
+namespace MyCustomWorkFlows
+{
+    internal class GetTaxWorkflow : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            //Create the tracing service
+            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+
+            //Create the context
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+
+        }
+    }
+}
